20e8235ff1a3e1aeb1018a82eee75431682ff15c
samdclopez <samdl8116@gmail.com>
2024-07-02 19:54:58 -0600

    Changes UI

--- app.js
+++ app.js
@@ -220,14 +220,14 @@ function updateTransactionHistory() {
         listItem.innerHTML = `
             <div class="flex-grow-1 d-flex flex-column bg-light p-3 border rounded-3 shadow-sm">
                 <div class="d-flex justify-content-between align-items-center">
-                    <span class="badge ${badge}">${type}</span>
-                    <span class="fw-bold">$${expense.amount}</span>
+                    <span class="badge ${badge} fs-4 fs-sm-3 fs-md-4 fs-lg-1 my-4">${type}</span>
+                    <span class="fs-4 fs-sm-3 fs-md-4 fs-lg-1 my-4 fw-bold ">$${expense.amount}</span>
                 </div>
                 <h6 class="mt-2 mb-1">${selection}</h6>
                 <div class="d-flex justify-content-between align-items-center">
-                    <p class="text-muted mb-0">${expense.description}</p>
-                    <button type="button" class="btn btn-danger btn-sm" onclick="deleteTransaction(${expense.id})">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                    <p class="fs-4 fs-sm-3 fs-md-4 fs-lg-1 my-4 text-muted mb-0">${expense.description}</p>
+                    <button type="button" class="btn btn-danger btn-lg" onclick="deleteTransaction(${expense.id})">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-trash" viewBox="0 0 20 20">
                             <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
                             <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
                         </svg>
